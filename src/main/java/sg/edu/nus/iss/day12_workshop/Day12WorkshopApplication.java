147568981cc4ff4955eba7e9884c2fa820d2d212
zh92 <zhehao92@gmail.com>
2023-02-07 10:03:58 +0800

    Initial commit

---
+++ src/main/java/sg/edu/nus/iss/day12_workshop/Day12WorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day12_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day12_workshop/Day12WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day12_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
